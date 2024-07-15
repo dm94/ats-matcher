0cf5be25333dce8efb6ffea1659ae1f6b2687d2a
dm94 <dm94@hotmail.es>
2024-07-15 21:35:44 +0200

    feat: Added the accordion

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { AddEducation } from "./_components/add-education";
 import { AddExperiencie } from "./_components/add-experiencie";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
 import { Education, SocialLink, Position } from "@/types/CurriculumVitae";
 
 export default function Builder() {
@@ -33,11 +38,8 @@ export default function Builder() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-3">
-      <form onSubmit={onSubmit} className="flex flex-col gap-6">
+      <form onSubmit={onSubmit} className="flex flex-col gap-6 w-full max-w-xl">
         <div className="flex flex-col gap-4" id="header-part">
-          <h2 className="text-md font-bold text-gray-900 dark:text-white">
-            Header
-          </h2>
           <div className="flex flex-col gap-2">
             <Label htmlFor="full_name">Full name *</Label>
             <Input
@@ -52,58 +54,70 @@ export default function Builder() {
             <Input type="text" id="position" placeholder="Developer" required />
           </div>
         </div>
-        <div className="flex flex-col gap-4" id="contact-part">
-          <h2 className="text-md font-bold text-gray-900 dark:text-white">
-            Contact
-          </h2>
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="email">Email *</Label>
-            <Input
-              type="email"
-              id="email"
-              placeholder="email@email.com"
-              required
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="address">Address</Label>
-            <Input type="text" id="address" placeholder="Street..." />
-          </div>
-          <div className="grid gap-6 md:grid-cols-2">
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="city">City</Label>
-              <Input type="text" id="city" placeholder="Talavera..." />
-            </div>
-            <div className="flex flex-col gap-2">
-              <Label htmlFor="phone_number">Phone Number</Label>
-              <Input type="tel" id="phone_number" placeholder="666..." />
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-2" id="about-part">
-          <Label htmlFor="about">About</Label>
-          <div className="flex flex-col gap-2">
-            <Textarea id="about" placeholder="I'am...." />
-          </div>
-        </div>
-        <div className="flex flex-col gap-4" id="education-part">
-          <h2 className="text-md font-bold text-gray-900 dark:text-white">
-            Education
-          </h2>
-          <AddEducation onAdd={addEducation} />
-        </div>
-        <div className="flex flex-col gap-4" id="experiencie-part">
-          <h2 className="text-md font-bold text-gray-900 dark:text-white">
-            Experiencie
-          </h2>
-          <AddExperiencie onAdd={addExperiencie} />
-        </div>
-        <div className="flex flex-col gap-4" id="projects-part">
-          <h2 className="text-md font-bold text-gray-900 dark:text-white">
-            Projects
-          </h2>
-          <AddExperiencie onAdd={addProject} />
-        </div>
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="contact" className="border-blue-100">
+            <AccordionTrigger>Contact</AccordionTrigger>
+            <AccordionContent>
+              <div className="flex flex-col gap-4" id="contact-part">
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="email">Email *</Label>
+                  <Input
+                    type="email"
+                    id="email"
+                    placeholder="email@email.com"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Label htmlFor="address">Address</Label>
+                  <Input type="text" id="address" placeholder="Street..." />
+                </div>
+                <div className="grid gap-6 md:grid-cols-2">
+                  <div className="flex flex-col gap-2">
+                    <Label htmlFor="city">City</Label>
+                    <Input type="text" id="city" placeholder="Talavera..." />
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <Label htmlFor="phone_number">Phone Number</Label>
+                    <Input type="tel" id="phone_number" placeholder="666..." />
+                  </div>
+                </div>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="about" className="border-blue-100">
+            <AccordionTrigger>About</AccordionTrigger>
+            <AccordionContent>
+              <div className="flex flex-col gap-2" id="about-part">
+                <Textarea id="about" placeholder="I'am...." />
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="education" className="border-blue-100">
+            <AccordionTrigger>Education</AccordionTrigger>
+            <AccordionContent>
+              <div className="flex flex-col gap-4" id="education-part">
+                <AddEducation onAdd={addEducation} />
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="experiencie" className="border-blue-100">
+            <AccordionTrigger>Experiencie</AccordionTrigger>
+            <AccordionContent>
+              <div className="flex flex-col gap-4" id="experiencie-part">
+                <AddExperiencie onAdd={addExperiencie} />
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="projects" className="border-blue-100">
+            <AccordionTrigger>Projects</AccordionTrigger>
+            <AccordionContent>
+              <div className="flex flex-col gap-4" id="projects-part">
+                <AddExperiencie onAdd={addProject} />
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </form>
     </main>
   );

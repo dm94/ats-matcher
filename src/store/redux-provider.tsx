02a8b02eb5f2ab86e93ecc96553f528daae5a9d1
dm94 <dm94@hotmail.es>
2024-07-15 23:17:19 +0200

    refactor: Removed redux-persist

--- src/store/redux-provider.tsx
+++ src/store/redux-provider.tsx
@@ -2,9 +2,7 @@
 
 import { Provider } from "react-redux";
 import { store } from "./index";
-import { persistStore } from "redux-persist";
 
-persistStore(store);
 export default function ReduxProvider({
   children,
 }: {

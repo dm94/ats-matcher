79621b28bfd267958da7daedb67bea38803f0bbf
dm94 <dm94@hotmail.es>
2024-07-18 23:28:16 +0200

    fix: Fixed the build for the pdfviewer error

--- src/components/pdf/viewer.tsx
+++ src/components/pdf/viewer.tsx
@@ -1,7 +1,16 @@
 "use client";
 
-import { PDFViewer } from "@react-pdf/renderer";
 import { Resume } from "./resume";
+
+import dynamic from "next/dynamic";
+
+const PDFViewer = dynamic(
+  () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
+  {
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+  }
+);
 
 export const Viewer = () => {
   return (
